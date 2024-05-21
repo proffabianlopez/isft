b3e38ce4007fb9254aa6615a8d05892c63f2d6aa
Arielito <arielmega6@gmail.com>
2024-05-20 23:33:15 -0300

    los css y las liberias de admin tle

---
+++ web/isft177/app/views/modules/head.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>ISFT nº 177</title>
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome Icons -->
+<link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css"> 
+<script src="./js/c5b742650e.js"></script>
+<!-- Theme style -->
+<link rel="stylesheet" href="./dist/css/adminlte.min.css">  
+<!-- DataTables -->
+<link rel="stylesheet" href="./plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="./plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="./plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+<!-- jQuery -->
+<script src="./js/jquery-3.5.1.slim.min.js"></script>
