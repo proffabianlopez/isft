ec3d251c61093ef47bfc12e47fe1513e3f7e04e1
Arielito <arielmega6@gmail.com>
2024-05-21 20:11:57 -0300

    commit

---
+++ web/isft177/app/views/modules/footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer d-flex p-1 align-items-center">  
+    <img src="./img/isft177_logo.png" alt="logo Instituto" class="ml-2 mr-4" height="40">
+    <strong class="mt-2">ISFT nº 177</strong>
+</footer>
