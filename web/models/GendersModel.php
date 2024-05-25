775dfb84f7295d6dfb4a7b842db11f85d5914182
MV <mev_tec@yahoo.com.ar>
2024-05-25 18:25:35 -0300

    modelo y controlador de gender

---
+++ web/models/GendersModel.php
@@ -0,0 +1,28 @@
+<?php 
+include_once 'config/MysqlDb.php'; // Asegúrate de incluir el archivo que contiene la clase model_sql
+
+class GendersModel {
+
+	static public function genders() {
+		$sql = ' 
+		SELECT genders.id_gender AS "id_gender"
+			,genders.details AS "details"
+		FROM genders;
+		';
+		$stmt = model_sql::connectToDatabase()->prepare($sql);
+
+		if($stmt->execute()) {
+	
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		} else {
+
+			print_r($stmt -> errorInfo());
+
+		}		
+		
+		$stmt = null;
+	
+	}
+
+}
