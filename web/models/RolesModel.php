e5b5c2c979b889a6b7040a76402f6afba1400b63
MV <mev_tec@yahoo.com.ar>
2024-05-26 22:33:44 -0300

    modificacion para mostrar roles 1 y 2

--- web/models/RolesModel.php
+++ web/models/RolesModel.php
@@ -5,24 +5,21 @@ class RolesModel {
 
 	static public function roles() {
 		$sql = ' 
-		SELECT roles.id_rol AS "id_rol"
-			,roles.name AS "name"
-		FROM roles;
+		SELECT roles.id_rol AS "id_rol",
+			roles.name AS "name"
+		FROM roles 
+		WHERE id_rol = 1 OR id_rol = 2;
 		';
 		$stmt = model_sql::connectToDatabase()->prepare($sql);
-
+	
 		if($stmt->execute()) {
-	
 			return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 		} else {
-
 			print_r($stmt -> errorInfo());
-
-		}		
-		
-		$stmt = null;
+		}        
 	
+		$stmt = null;
 	}
+	
 
 }
