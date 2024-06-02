d51cbbf2d7fb05a03bddf8052f0abedac1451c35
onunezz <alegusnunez@gmail.com>
2024-06-02 00:57:31 -0300

    corrigiendo botón para esconder el sidebar

--- web/views/modules/navbar.php
+++ web/views/modules/navbar.php
@@ -8,30 +8,30 @@
     </ul>
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-    
-      <!-- Username con dropdown para "Mis datos" -->
-      <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-user"></i> 
-        <?php $data = UserController::sessionDataUser($_SESSION['id_user'])?>
-        <?php echo $data['name_user'] . " " . $data['last_name_user']?> 
-    </a>
-    <?php if($data['change_password'] != 0): ?>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-        <a class="dropdown-item" href="index.php?pages=myData">
-            <i class="fas fa-id-card"></i> Mis datos
-        </a>
-        <a class="dropdown-item" href="index.php?pages=changePassword">
-            <i class="fas fa-key"></i> Cambiar Contraseña
-        </a>
-    </div>
-</li>
+
+        <!-- Username con dropdown para "Mis datos" -->
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i>
+                <?php $data = UserController::sessionDataUser($_SESSION['id_user']) ?>
+                <?php echo $data['name_user'] . " " . $data['last_name_user'] ?>
+            </a>
+            <?php if ($data['change_password'] != 0) : ?>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="index.php?pages=myData">
+                        <i class="fas fa-id-card"></i> Mis datos
+                    </a>
+                    <a class="dropdown-item" href="index.php?pages=changePassword">
+                        <i class="fas fa-key"></i> Cambiar Contraseña
+                    </a>
+                </div>
+        </li>
 
 
-    
-    <!-- Botón para ocultar la barra lateral -->
+
+        <!-- Botón para ocultar la barra lateral -->
         <li class="nav-item">
-            <button id="toggle-sidebar-btn" class="nav-link" role="button" style="background-color: transparent; border: none;" title="Ocultar Barra Lateral">
+            <button id="toggle-sidebar-btn" class="nav-link d-none d-lg-inline" role="button" style="background-color: transparent; border: none;" title="Ocultar Barra Lateral">
                 <i class="fas fa-angle-left" style="margin-right: 5px;"></i> <!-- Icono de flecha hacia la izquierda -->
                 <i class="fas fa-angle-right"></i> <!-- Icono de flecha hacia la derecha -->
             </button>
@@ -42,7 +42,7 @@
                 <i class="fas fa-power-off"></i> <!-- Icono de apagado para cerrar sesión -->
             </a>
         </li>
-        <?php endif; ?>
+    <?php endif; ?>
     </ul>
 
 </nav>
