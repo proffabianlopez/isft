55728a5a61f1ab9ad9a14cd96365cb65b0ea2541
Fabrusio <fabrusio3p3@gmail.com>
2024-06-05 23:14:25 -0300

    funciones para traer los datos de la BD y para editarlos

--- web/models/TeacherModel.php
+++ web/models/TeacherModel.php
@@ -19,5 +19,54 @@ class TeacherModel extends UserModel {
             print_r($stmt->errorInfo());
         }
     }
+
+    static public function getAllTeachers(){
+        $sql="SELECT
+        users.id_user As id_teacher,
+        users.name AS name_teacher,
+        users.last_name AS last_name_teacher,
+        users.email AS email_teacher,
+        users.dni AS dni,
+        users.fk_rol_id AS fk_rol_id
+        FROM users
+        WHERE 
+        users.fk_rol_id = 4
+        AND users.state IN (1, 2)";
+
+            $stmt = model_sql::connectToDatabase()->prepare($sql);
+
+                if($stmt->execute()) {
+
+                     return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                } else {
+
+                    print_r($stmt -> errorInfo());
+
+                    }		
+
+                $stmt = null;
+
+    }
+
+    static public function updateTeacherData($name, $last_name, $email, $id_teacher){
+        $sql = "UPDATE users SET name = :name, last_name = :last_name, email = :email WHERE id_user = :id_teacher";
+        $stmt = model_sql::connectToDatabase()->prepare($sql);
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+        $stmt->bindParam(':last_name', $last_name, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':id_teacher', $id_teacher, PDO::PARAM_INT);
+    
+        if($stmt->execute()) {		 
+            // Devolver true si la actualización se realiza correctamente
+            return true;
+        } else { 
+            // Manejar cualquier error que pueda ocurrir durante la ejecución de la consulta
+            print_r($stmt->errorInfo());
+            return false; // Devolver false en caso de error
+        }		
+        $stmt = null;
+    }
+
 }
 ?>
