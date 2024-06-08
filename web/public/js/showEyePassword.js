88fe4324e326c3891c53e447fda4417e1237f480
Arielito123 <arielmega6@gmail.com>
2024-06-08 17:45:47 -0300

    se linkeo el js y la logica para mostrar en login

---
+++ web/public/js/showEyePassword.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+
+    togglePassword.addEventListener('click', function() {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+        togglePassword.innerHTML = type === 'password' ? '<i class="bi bi-eye-slash"></i>' : '<i class="bi bi-eye"></i>';
+    });
+});
