10d08d9117f25ae7445ef4ca7058d94a0a90efa4
Arielito123 <arielmega6@gmail.com>
2024-06-08 00:04:35 -0300

    se habia roto porque habia que cambiar el select de editar user a CareerController y estaba en carrer

--- web/views/subfolder/listStudent.php
+++ web/views/subfolder/listStudent.php
@@ -1,52 +1,51 @@
-<?php $dataStudent=StudentController::getAllStudent();?>
+<?php $dataStudent = StudentController::getAllStudent(); ?>
 <div class="card">
     <div class="card-body">
         <div class="table-responsive">
             <table id="example3" class="table table-bordered table-striped table-hover custom-table-container" style="width: 80%; margin: 0 auto;">
-                <thead class="bg-yellow text-white"> 
-                    <tr class="text-center">            
+                <thead class="bg-yellow text-white">
+                    <tr class="text-center">
                         <th>Nombre</th>
                         <th>Apellido</th>
                         <th>Acciones</th>
                     </tr>
                 </thead>
-                <tbody>   
+                <tbody>
                     <?php foreach ($dataStudent as $student): ?>
                         <tr>
                             <td class="text-center"><?php echo $student['name_student']; ?></td>
                             <td class="text-center"><?php echo $student['last_name_student']; ?></td>
                             <?php if (isset($_GET['pages']) && ($_GET['pages'] == 'manageStudent')): ?>
-                            <td class="text-center">
-                                <a href="#viewUserModal<?php echo $student['id_student']; ?>" class="btn btn-success view-user" data-toggle="modal" title="Ver detalles">
-                                    <i class="fas fa-eye"></i>
-                                </a>
+                                <td class="text-center">
+                                    <a href="#viewUserModal<?php echo $student['id_student']; ?>" class="btn btn-success view-user" data-toggle="modal" title="Ver detalles">
+                                        <i class="fas fa-eye"></i>
+                                    </a>
 
-                                <a href="#confirmDeleteModal<?php echo $student['id_student']; ?>" class="btn btn-danger delete-user" data-toggle="modal" title="eliminar">
-                                    <i class="fas fa-trash-alt"></i>
-                                </a>
-                                <a href="#editUserModal<?php echo $student['id_student']; ?>" class="btn btn-primary edit-user" data-toggle="modal" title="editar">
-                                    <i class="fas fa-edit"></i>
-                                </a>
+                                    <a href="#confirmDeleteModal<?php echo $student['id_student']; ?>" class="btn btn-danger delete-user" data-toggle="modal" title="Eliminar">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </a>
+                                    <a href="#editUserModal<?php echo $student['id_student']; ?>" class="btn btn-primary edit-user" data-toggle="modal" title="Editar">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
 
-                                <a href="index.php?pages=manageStudent&action=activar_cuenta&id_user=<?php echo $user['id_user']?>" class="btn btn-info" title="Generar Cuenta Usuario">
-                                    <i class="fas fa-user-plus"></i>
-                                </a>
-                                
-                            </td>
+                                    <a href="index.php?pages=manageStudent&action=activar_cuenta&id_user=<?php echo $student['id_student']; ?>" class="btn btn-info" title="Generar Cuenta Usuario">
+                                        <i class="fas fa-user-plus"></i>
+                                    </a>
+
+                                </td>
                             <?php endif; ?>
                         </tr>
                     <?php endforeach; ?>
-                </tbody>                
+                </tbody>
             </table>
         </div>
     </div>
 </div>
 
-
 <?php foreach ($dataStudent as $student): ?>
- 
+
     <!-- Modal eliminar usuario -->
-<div class="modal fade" id="confirmDeleteModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+    <div class="modal fade" id="confirmDeleteModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header alert alert-danger">
@@ -70,11 +69,10 @@
             </div>
         </div>
     </div>
-</div>
+    </div>
 
-
-<!-- Modal de edición de usuario -->
-<div class="modal fade" id="editUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true">
+    <!-- Modal de edición de usuario -->
+    <div class="modal fade" id="editUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header alert alert-warning">
@@ -102,7 +100,7 @@
                     <div class="form-group">
                         <label for="roles">Carrera</label>
                         <select class="form-control" id="carrer" name="carrer" required>
-                            <?php (new CarrerController())->carrerSelect();?>
+                            <?php (new CareerController())->careerSelect();?>
                         </select>
                     </div>
                     <button type="submit" name="savechange" class="btn btn-warning">Guardar cambios</button>
@@ -110,10 +108,10 @@
             </div>
         </div>
     </div>
-</div>
+    </div>
 
-<!-- Modal de vista de usuario -->
-<div class="modal fade" id="viewUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="viewUserModalLabel" aria-hidden="true">
+    <!-- Modal de vista de usuario -->
+    <div class="modal fade" id="viewUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="viewUserModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header alert alert-success">
@@ -134,22 +132,21 @@
             </div>
         </div>
     </div>
-</div>
-
+    </div>
 
 <?php endforeach; ?>
 
 <?php
 if (isset($_POST['del_student'])) {
-    $controller= new StudentController();
+    $controller = new StudentController();
     $controller->eliminatedStudent();
 }
 ?>
 
-<?php  
+<?php
 if (isset($_POST['savechange'])) {
-    $controller= new StudentController();
+    $controller = new StudentController();
     $controller->editStudent();
-    
+
 }
 ?>
