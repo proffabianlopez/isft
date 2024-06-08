c50647dbd1a61ec8387f4cd82a073d3c7613d7ca
Arielito123 <arielmega6@gmail.com>
2024-06-08 17:52:17 -0300

    se modifico el js para que tome por clase

--- web/public/js/showEyePassword.js
+++ web/public/js/showEyePassword.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const togglePassword = document.getElementById('togglePassword');
-    const passwordInput = document.getElementById('password');
+    const togglePassword = document.querySelector('.togglePassword');
+    const passwordInput = document.querySelector('.password');
 
     togglePassword.addEventListener('click', function() {
         const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
