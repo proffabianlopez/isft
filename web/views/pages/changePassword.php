7fee38a12a5489c30f716ede90daa8e7d3d60059
MV <mev_tec@yahoo.com.ar>
2024-06-09 18:47:18 -0300

    correccion visual change password

--- web/views/pages/changePassword.php
+++ web/views/pages/changePassword.php
@@ -1,32 +1,34 @@
 <div class="container d-flex justify-content-center align-items-center min-vh-100">
     <div class="card w-100" style="max-width: 400px;">
-      <div class="card-header text-center text-primary fs-4 fw-bold">
-        Cambiar Contraseña
-      </div>
-      <div class="card-body">
-        <form method="POST">
-          <div class="mb-3">
-            <label for="currentPassword" class="form-label fw-bold">Contraseña Actual</label>
-            <input type="password" class="form-control border-primary" name="currentPassword" placeholder="Introduce tu contraseña actual">
-          </div>
-          <div class="mb-3">
-            <label for="newPassword" class="form-label fw-bold">Nueva Contraseña</label>
-            <input type="password" class="form-control border-primary" name="newPassword" placeholder="Introduce tu nueva contraseña">
-          </div>
-          <div class="mb-3">
-            <label for="confirmPassword" class="form-label fw-bold">Confirmar Nueva Contraseña</label>
-            <input type="password" class="form-control border-primary" name="confirmPassword" placeholder="Confirma tu nueva contraseña">
-          </div>
-          <button type="submit" name="enviar" class="btn btn-primary w-100">Cambiar Contraseña</button>
-        </form>
-        <?php
-        
-        if(isset($_POST['enviar'])){
-          $controller = new UserController();
-          $controller->newPassword();
-        }
+        <div class="card-header text-center bg-warning text-dark fs-6 fw-bold">
+            <span class="fs-5 font-weight-bold">
+            <h4 class="my-1 font-weight-bold">Cambiar Contraseña</h4>
+            </span>
+        </div>
+        <div class="card-body">
+            <form method="POST">
+                <div class="mb-3">
+                    <label for="currentPassword" class="form-label fw-bold">Contraseña Actual</label>
+                    <input type="password" class="form-control border-primary" name="currentPassword" placeholder="Introduce tu contraseña actual">
+                </div>
+                <div class="mb-3">
+                    <label for="newPassword" class="form-label fw-bold">Nueva Contraseña</label>
+                    <input type="password" class="form-control border-primary" name="newPassword" placeholder="Introduce tu nueva contraseña">
+                </div>
+                <div class="mb-3">
+                    <label for="confirmPassword" class="form-label fw-bold">Confirmar Nueva Contraseña</label>
+                    <input type="password" class="form-control border-primary" name="confirmPassword" placeholder="Confirma tu nueva contraseña">
+                </div>
+                <button type="submit" name="enviar" class="btn btn-warning text-dark w-100">Cambiar Contraseña</button>
+            </form>
+            <?php
+            
+            if(isset($_POST['enviar'])){
+                $controller = new UserController();
+                $controller->newPassword();
+            }
 
-        ?>
-      </div>
+            ?>
+        </div>
     </div>
-  </div>
+</div>
