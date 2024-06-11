513da410ed2335069c15d77a0b5107c84f486b15
Arielito123 <arielmega6@gmail.com>
2024-06-10 22:27:32 -0300

    agregado modal career

--- web/views/pages/allCareers.php
+++ web/views/pages/allCareers.php
@@ -1,3 +1,4 @@
+
 <?php
 $data = CareerController::getCareersData();
 ?>
@@ -6,8 +7,7 @@ $data = CareerController::getCareersData();
     <div class="row py-4">
         <?php foreach ($data as $key => $value) : ?>
             <div class="col-lg-6">
-               
-                    <a href="index.php?pages=toolsCareer&id_career=<?php echo $value['id_career']?>&name_career=<?php echo $value['career_name'] ?>&state=<?php echo $value['state'] ?>"> 
+                <a href="index.php?pages=toolsCareer&id_career=<?php echo $value['id_career']?>&name_career=<?php echo $value['career_name'] ?>&state=<?php echo $value['state'] ?>"> 
                     <div class="small-box bg-secondary">
                         <?php if ($value['state'] == 'inactive') : ?>
                             <div class="ribbon-wrapper ribbon-xl">
@@ -27,8 +27,51 @@ $data = CareerController::getCareersData();
                             <b>Herramientas</b><i class="fas fa-tools ml-2"></i>
                         </div>
                     </div>
-                     </a>
+                </a>
             </div>
         <?php endforeach ?>
     </div>
 </section>
+
+<!-- Floating Button -->
+<button type="button" class="btn btn-primary btn-floating" data-toggle="modal" data-target="#createCareerModal" title="crear carrera">
+    <i class="fas fa-plus-circle"></i>
+</button>
+<div class="modal fade" id="createCareerModal" tabindex="-1" role="dialog" aria-labelledby="createCareerModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-warning text-white">
+                <h5 class="modal-title" id="createCareerModalLabel">Crear Nueva Carrera</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="createCareerForm" method="post">
+                    <div class="form-group">
+                        <label for="careerName">Nombre de la Carrera</label>
+                        <input type="text" class="form-control" id="careerName" name="careerName" placeholder="Ingrese el nombre de la carrera" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="careerTitle">Título</label>
+                        <input type="text" class="form-control" id="careerTitle" name="description" placeholder="Ingrese el título de la carrera" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="careerAbbreviation">Abreviación</label>
+                        <input type="text" class="form-control" id="careerAbbreviation" name="abbreviation" placeholder="Abreviatura para legajo" required>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="submit" form="createCareerForm" class="btn btn-warning" name="loadCareer">Guardar</button>
+            </div>
+            <?php
+                        if (isset($_POST['loadCareer'])) {
+                            $controller = new CareerController();
+                            $controller->newCareer();
+                        }
+                        ?>
+        </div>
+    </div>
+</div>
