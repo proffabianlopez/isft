5acb98ed308cb38dd691f143cc56c4b8b236d666
onunezz <alegusnunez@gmail.com>
2024-06-15 00:58:04 -0300

    correlativas parte 2

--- web/views/subfolder/newCorrelative.php
+++ web/views/subfolder/newCorrelative.php
@@ -3,7 +3,6 @@ if ((isset($_GET['name_career'])) && (isset($_GET['id_career'])) && (isset($_GET
     $correlatives = CorrelativeController::listCorrelative($_GET["id_career"]);
 ?>
     <div class="d-flex justify-content-center align-items-center p-5">
-
         <div class="card card-primary mt-2 mb-2 w-50">
             <div class="card-body">
                 <form method="post">
@@ -32,13 +31,12 @@ if ((isset($_GET['name_career'])) && (isset($_GET['id_career'])) && (isset($_GET
                 </form>
             </div>
         </div>
-
     </div>
     <div class="d-flex justify-content-center">
         <div class="card w-75">
             <div class="card-body">
-                <table class="table table-bordered table-striped table-responsive-sm" id="example2">
-                    <thead>
+                <table id="example3" class="table table-bordered table-striped table-responsive-sm">
+                    <thead class="bg-custom">
                         <tr>
                             <th>Para rendir...</th>
                             <th>Debe aprobar...</th>
@@ -48,38 +46,103 @@ if ((isset($_GET['name_career'])) && (isset($_GET['id_career'])) && (isset($_GET
                     <tbody>
                         <?php foreach ($correlatives as $correlative) : ?>
                             <tr>
-                                <td hidden><?php echo $correlative["id_subject"]; ?></td>
                                 <td><?php echo htmlspecialchars($correlative['name_subject']) ?></td>
+                                <td><?php echo htmlspecialchars($correlative['name_correlative_subject']) ?></td>
                                 <td>
-                                    <?php
-                                    $info = CorrelativeController::listMultipleCorrelatives($correlative['id_subject']);
-                                    if (!empty($info)) {
-                                        $correlativesList = [];
-                                        foreach ($info as $correlation) {
-                                            $correlativesList[] = htmlspecialchars($correlation['correlatives']);
-                                        }
-                                        echo implode(' - ', $correlativesList);
-                                    }
-                                    ?>
-                                </td>
-                                <td>
-                                    <a href="index.php?pagina=carrerasCorrelatividadMenu&correlatividad=<?php echo base64_encode($correlative['id_correlative']) ?>&carrera=<?php echo $_GET['id_career'] ?>&name=<?php echo $_GET['name_career'] ?>&para=<?php echo base64_encode($correlative['id_subject']) ?>&paraid=<?php echo base64_encode($correlative['id_correlative_subject']) ?>&debe=<?php echo base64_encode($correlative['id_subject']) ?>&debeid=<?php echo base64_encode($correlative['id_correlative_subject']) ?>&submenu=correlatividadEditar&estate=<?php echo $_GET['state'] ?>" class="btn btn-warning"><i class="fas fa-edit"></i></a>
-                                    <a href="index.php?pagina=carrerasCorrelatividadMenu&correlatividad=<?php echo base64_encode($correlative['id_correlative']) ?>&carrera=<?php echo $_GET['id_career'] ?>&name=<?php echo $_GET['name_career'] ?>&para=<?php echo base64_encode($correlative['id_subject']) ?>&debe=<?php echo base64_encode($correlative['id_subject']) ?>&submenu=correlatividadEliminar&estate=<?php echo $_GET['state'] ?>" class="btn btn-danger"><i class="fas fa-trash-alt"></i></a>
+                                    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modal_edit_<?php echo $correlative['id_correlative'] ?>" title="editar materia">
+                                        <i class="fas fa-edit"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirmDeleteModal_<?php echo $correlative['id_correlative'] ?>">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
                                 </td>
                             </tr>
                         <?php endforeach ?>
-
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
-
-
-
     <?php if (isset($_POST["loadCorrelative"])) {
         $controller = new CorrelativeController();
         $controller->newCorrelative($_GET["id_career"], $_GET["name_career"], $_GET["state"]);
     } ?>
-<?php
-} // cierre
+<?php } ?>
+<?php foreach ($correlatives as $correlative) : ?>
+    <div class="modal fade" id="modal_edit_<?php echo $correlative['id_correlative'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-warning">
+                    <h5 class="modal-title" id="exampleModalLabel">Editar Correlativa</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="post">
+                        <input type="hidden" name="id_correlative" value="<?php echo $correlative['id_correlative'] ?>">
+                        <div class="form-group">
+                            <label for="para_rendir">Para rendir...(Materia)</label>
+                            <select class="custom-select" name="toRender">
+                                <option value="<?php echo $correlative['id_subject'] ?>"><?php echo $correlative["name_subject"] ?></option>
+                                <?php
+                                (new CorrelativeController())->correlativeSelect($_GET["id_career"]);
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="debe_aprobar">Debe aprobar...(Correlativa)</label>
+                            <select class="custom-select" name="subjectApproved">
+                                <option value="<?php echo $correlative['id_subject'] ?>"><?php echo $correlative["name_correlative_subject"] ?></option>
+                                <?php
+                                (new CorrelativeController())->correlativeSelect($_GET["id_career"]);
+                                ?>
+                            </select>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-warning" name="savechange">Guardar Cambios</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endforeach ?>
+
+<?php foreach ($correlatives as $correlative) : ?>
+    <div class="modal fade" id="confirmDeleteModal_<?php echo $correlative['id_correlative'] ?>" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered " role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-danger text-white">
+                    <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar Eliminación</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p class="text-center">¿Estás seguro de que deseas eliminar la siguiente correlativa?</p>
+                    <h5 class="text-center mt-4 mb-4"><?php echo $correlative['name_subject'] . " -> " . $correlative["name_correlative_subject"] ?></h5>
+                    <p class="text-center">Esta acción no se puede deshacer.</p>
+                    <form method="post">
+                        <input type="hidden" name="id_correlative" value="<?php echo $correlative['id_correlative'] ?>">
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                            <button type="submit" class="btn btn-danger" name="deleteButton">Eliminar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endforeach ?>
+
+<?php if (isset($_POST["savechange"])) {
+    $controller = new CorrelativeController();
+    $controller->editCorrelative($_GET["id_career"], $_GET["name_career"], $_GET["state"]);
+}
+if (isset($_POST["deleteButton"])) {
+    $controller = new CorrelativeController();
+    $controller->deleteCorrelative($_GET["id_career"], $_GET["name_career"], $_GET["state"]);
+}
+?>
