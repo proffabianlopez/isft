9b7c0fcb90f4c9389b513144e1b115dad82e0f4a
Arielito123 <arielmega6@gmail.com>
2024-07-01 14:16:14 -0300

    modalAndando

--- web/views/subfolder/listStudent.php
+++ web/views/subfolder/listStudent.php
@@ -51,8 +51,8 @@
 <?php foreach ($dataStudent as $student) : ?>
 
 
-    <!-- modal de asignar legajo -->
-    <div id="assignFileModal<?php echo $student['id_student']; ?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="assignFileModalLabel<?php echo $student['id_student']; ?>" aria-hidden="true">
+   <!-- modal de asignar legajo -->
+<div id="assignFileModal<?php echo $student['id_student']; ?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="assignFileModalLabel<?php echo $student['id_student']; ?>" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header bg-custom text-black ">
@@ -71,7 +71,7 @@
                         <small class="form-text text-muted">El número de legajo debe ser un número de 4 dígitos.</small>
                     </div>
                     <div class="modal-footer">
-                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
                         <button type="submit" class="btn btn-warning" name="save">Guardar</button>
                     </div>
                 </form>
@@ -82,42 +82,41 @@
 
 
 
-    <!-- Modal de edición de usuario -->
-    <div class="modal fade" id="editUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header alert alert-warning">
-                    <h5 class="modal-title" id="editUserModalLabel"><strong>Editar alumno</strong></h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form method="POST">
-                        <input type="hidden" name="id_student" value="<?php echo $student['id_student']; ?>">
-                        <input type="hidden" name="id_career_person" value="<?php echo $student['id_career_person']; ?>">
-                        <div class="form-group">
-                            <label for="apellido">Apellido</label>
-                            <input type="text" class="form-control" id="last_name_student" pattern="[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+" title="Solo se permiten letras y espacios" name="last_name_student" value="<?php echo $student['last_name_student']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="nombre">Nombre</label>
-                            <input type="text" class="form-control" id="nam" pattern="[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+" title="Solo se permiten letras y espacios" name="name_student" required value="<?php echo $student['name_student']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="roles">Carrera</label>
-                            <select class="form-control" id="carrer" name="carrer" required>
-                                <option value="<?php echo $student['id_career'] ?>"><?php echo $student["career_name"] ?></option>
-                                <?php (new CareerController())->careerSelect(); ?>
-                            </select>
-                        </div>
-                        <button type="submit" name="savechange" class="btn btn-warning">Guardar cambios</button>
-                    </form>
-                </div>
+
+   <!-- Modal de edición de usuario -->
+<div class="modal fade" id="editUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header alert alert-warning">
+                <h5 class="modal-title" id="editUserModalLabel"><strong>Editar alumno</strong></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST">
+                    <input type="hidden" name="id_student" value="<?php echo $student['id_student']; ?>">
+                    <input type="hidden" name="id_career_person" value="<?php echo $student['id_career_person']; ?>">
+                    <div class="form-group">
+                        <label for="apellido">Apellido</label>
+                        <input type="text" class="form-control" id="last_name_student" pattern="[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+" title="Solo se permiten letras y espacios" name="last_name_student" value="<?php echo $student['last_name_student']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" id="nam" pattern="[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+" title="Solo se permiten letras y espacios" name="name_student" required value="<?php echo $student['name_student']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="roles">Carrera</label>
+                        <select class="form-control" id="carrer" name="carrer" required>
+                            <?php (new CareerController())->careerSelect($student['id_career']); ?>
+                        </select>
+                    </div>
+                    <button type="submit" name="savechange" class="btn btn-warning">Guardar cambios</button>
+                </form>
             </div>
         </div>
     </div>
-
+</div>
     <!-- Modal de vista de usuario -->
     <div class="modal fade" id="viewUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="viewUserModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -223,8 +222,9 @@ if (isset($_GET['action'])) {
 <?php foreach ($dataStudent as $student) : ?>
 
 
-    <!-- modal de asignar legajo -->
-    <div id="assignFileModal<?php echo $student['id_student']; ?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="assignFileModalLabel<?php echo $student['id_student']; ?>" aria-hidden="true">
+   
+   <!-- modal de asignar legajo -->
+<div id="assignFileModal<?php echo $student['id_student']; ?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="assignFileModalLabel<?php echo $student['id_student']; ?>" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header bg-custom text-black ">
@@ -243,7 +243,7 @@ if (isset($_GET['action'])) {
                         <small class="form-text text-muted">El número de legajo debe ser un número de 4 dígitos.</small>
                     </div>
                     <div class="modal-footer">
-                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
                         <button type="submit" class="btn btn-warning" name="save">Guardar</button>
                     </div>
                 </form>
@@ -254,42 +254,41 @@ if (isset($_GET['action'])) {
 
 
 
-    <!-- Modal de edición de usuario -->
-    <div class="modal fade" id="editUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header alert alert-warning">
-                    <h5 class="modal-title" id="editUserModalLabel"><strong>Editar alumno</strong></h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form method="POST">
-                        <input type="hidden" name="id_student" value="<?php echo $student['id_student']; ?>">
-                        <input type="hidden" name="id_career_person" value="<?php echo $student['id_career_person']; ?>">
-                        <div class="form-group">
-                            <label for="apellido">Apellido</label>
-                            <input type="text" class="form-control" id="last_name_student" pattern="[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+" title="Solo se permiten letras y espacios" name="last_name_student" value="<?php echo $student['last_name_student']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="nombre">Nombre</label>
-                            <input type="text" class="form-control" id="nam" pattern="[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+" title="Solo se permiten letras y espacios" name="name_student" required value="<?php echo $student['name_student']; ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="roles">Carrera</label>
-                            <select class="form-control" id="carrer" name="carrer" required>
-                                <option value="<?php echo $student['id_career'] ?>"><?php echo $student["career_name"] ?></option>
-                                <?php (new CareerController())->careerSelect(); ?>
-                            </select>
-                        </div>
-                        <button type="submit" name="savechange" class="btn btn-warning">Guardar cambios</button>
-                    </form>
-                </div>
+
+   <!-- Modal de edición de usuario -->
+<div class="modal fade" id="editUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header alert alert-warning">
+                <h5 class="modal-title" id="editUserModalLabel"><strong>Editar alumno</strong></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST">
+                    <input type="hidden" name="id_student" value="<?php echo $student['id_student']; ?>">
+                    <input type="hidden" name="id_career_person" value="<?php echo $student['id_career_person']; ?>">
+                    <div class="form-group">
+                        <label for="apellido">Apellido</label>
+                        <input type="text" class="form-control" id="last_name_student" pattern="[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+" title="Solo se permiten letras y espacios" name="last_name_student" value="<?php echo $student['last_name_student']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" id="nam" pattern="[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+" title="Solo se permiten letras y espacios" name="name_student" required value="<?php echo $student['name_student']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="roles">Carrera</label>
+                        <select class="form-control" id="carrer" name="carrer" required>
+                            <?php (new CareerController())->careerSelect($student['id_career']); ?>
+                        </select>
+                    </div>
+                    <button type="submit" name="savechange" class="btn btn-warning">Guardar cambios</button>
+                </form>
             </div>
         </div>
     </div>
-
+</div>
     <!-- Modal de vista de usuario -->
     <div class="modal fade" id="viewUserModal<?php echo $student['id_student']; ?>" tabindex="-1" role="dialog" aria-labelledby="viewUserModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
